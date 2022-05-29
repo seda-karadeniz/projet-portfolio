445c06e527bb7b16aa66e28b3d6cedcd511dcc59
seda-karadeniz <72101144+seda-karadeniz@users.noreply.github.com>
2022-05-29 17:38:07 +0200

    add

--- wp-content/themes/portfolio/partials/project.php
+++ wp-content/themes/portfolio/partials/project.php
@@ -7,7 +7,7 @@
             <div class="project__label">
                 <h3 class="project__title subTitle"><?= get_the_title(); ?></h3>
                 <p class="project__small-desc"><?= get_field('small_description'); ?></p>
-                <div class="arrow-left">
+                <div class="project__arrow-left">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25.242" viewBox="0 0 24 25.242" >
                         <g id="Icon_feather-arrow-left" data-name="Icon feather-arrow-left" transform="translate(1.5 2.121)">
                             <path id="Tracé_2" data-name="Tracé 2" d="M7.5,18h21" transform="translate(-7.5 -7.5)" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>

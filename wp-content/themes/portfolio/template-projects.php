6a1b4f0e0aa09ae8691196db450660085e330c32
seda-karadeniz <72101144+seda-karadeniz@users.noreply.github.com>
2022-05-31 14:27:42 +0200

    add

--- wp-content/themes/portfolio/template-projects.php
+++ wp-content/themes/portfolio/template-projects.php
@@ -2,7 +2,41 @@
 <?php get_header();?>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
     <main class="layout projects">
-        <h2 class="templateProjects__title title"> <?= get_the_title(); ?></h2>
+        <div class="wrap-title-svg">
+            <div class="title-svg">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="49.497" height="49.497" viewBox="0 0 49.497 49.497">
+                    <defs>
+                        <filter id="Rectangle_26">
+                            <feOffset input="SourceAlpha"/>
+                            <feGaussianBlur stdDeviation="3" result="blur"/>
+                            <feFlood flood-color="#ffafbd" result="color"/>
+                            <feComposite operator="out" in="SourceGraphic" in2="blur"/>
+                            <feComposite operator="in" in="color"/>
+                            <feComposite operator="in" in2="SourceGraphic"/>
+                        </filter>
+                    </defs>
+                    <g data-type="innerShadowGroup">
+                        <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Rectangle_26)">
+                            <rect id="Rectangle_26-2" data-name="Rectangle 26" width="35" height="35" transform="translate(24.75) rotate(45)" fill="#fff"/>
+                        </g>
+                        <g id="Rectangle_26-3" data-name="Rectangle 26" transform="translate(24.749) rotate(45)" fill="none" stroke="#ffafbd" stroke-width="2">
+                            <rect width="35" height="35" stroke="none"/>
+                            <rect x="1" y="1" width="33" height="33" fill="none"/>
+                        </g>
+                    </g>
+                    <animateTransform attributeName="transform"
+                                      attributeType="XML"
+                                      type="rotate"
+                                      from="0"
+                                      to="360"
+                                      dur="10s"
+                                      repeatCount="2"/>
+                </svg>
+
+            </div>
+            <h2 class="templateProjects__title title"> <?= get_the_title(); ?></h2>
+        </div>
+
         <section class="projects" id="projects">
             <h3 class="hidden">Mes projets</h3>
             <div class="projects__wrapper">
